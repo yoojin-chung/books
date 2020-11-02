9e2e3e198622aa44e095881f782df164a150167e
Yoojin Chung <yoojin.chung@gmail.com>
2020-11-01 20:44:08 -0500

    Initial commit of book recommender

---
+++ train_ratings.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 30 10:45:06 2020
+
+@author: yooji
+"""
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import pickle
+from scipy.optimize import minimize
+
+
+def cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lam):
+    """Compute cost and gradient for collaborative filtering."""
+    X = params[:num_movies*num_features].reshape(num_movies, num_features)
+    Theta = params[num_movies*num_features:].reshape(num_users, num_features)
+
+    J = np.sum((np.dot(X, Theta.T)*R - Y)**2)/2 +\
+        lam/2*(np.sum(Theta**2) + np.sum(X**2))
+
+    X_grad = (X.dot(Theta.T)*R-Y).dot(Theta) + lam*X
+    Theta_grad = (X.dot(Theta.T)*R-Y).T.dot(X) + lam*Theta
+    new_params = np.hstack([X_grad.flatten(), Theta_grad.flatten()])
+    return J, new_params
+
+
+def normRatings(Y, R):
+    """Normalize ratings."""
+    Ymean = np.sum(Y, axis=1)/np.sum(R, axis=1)
+    Ynorm = Y-Ymean[:, None]*R
+    return Ynorm, Ymean
+
+
+# %% Load data
+ratings = pd.read_csv('archive/ratings.csv')
+ratings['rating'] = ratings.rating.astype('uint8')
+ratings['book_id'] = ratings.book_id.astype('uint16')
+ratings['user_id'] = ratings.user_id.astype('uint16')
+print(ratings)
+
+# %% Cull data and divide to train/test sets
+bybook = ratings['rating'].groupby(ratings.book_id).count()
+byuser = ratings[['user_id', 'rating']].groupby('user_id').count()
+
+users_culled = byuser[byuser.rating >= 100].index.tolist()
+ratings_culled = ratings[ratings.user_id.isin(users_culled)]
+
+idx = np.random.rand(len(ratings_culled)) < 0.8
+ratings_train = ratings_culled[idx]
+ratings_test = ratings_culled[~idx]
+
+F = open('train_ind.pkl', 'wb')
+pickle.dump(idx, F)
+F.close()
+
+pivoted_train = ratings_train.pivot_table(index='book_id',
+                                          columns='user_id',
+                                          values='rating',
+                                          fill_value=0)
+ax = sns.heatmap(pivoted_train.sample(100))
+
+# %% Normalize data, initialize parameters
+train_mat = np.array(pivoted_train, dtype='float16')
+Y = train_mat
+R = Y!=0
+
+Ynorm, Ymean = normRatings(Y, R)
+num_movies, num_users = Y.shape
+num_features = 3
+
+X = np.random.randn(num_movies, num_features)
+Theta = np.random.randn(num_users, num_features)
+
+init_params = np.hstack([X.flatten(), Theta.flatten()])
+lam = 1
+X.shape
+num_movies, num_users
+
+# %%
+theta = minimize(cofiCostFunc,
+                 x0=init_params,
+                 args=(Ynorm, R, num_users, num_movies, num_features, lam),
+                 method='TNC',
+                 jac=True)
+
+F = open('theta_trained.pkl', 'wb')
+pickle.dump(theta, F)
+F.close()
